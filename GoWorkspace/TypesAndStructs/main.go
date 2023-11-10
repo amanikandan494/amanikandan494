4ccf7cee3a7e39df9a7765f8482a10d604a5b4f8
anishm <anishm.m@infosys.com>
2023-11-10 19:45:28 +0530

    commits of 3 folders

---
+++ GoWorkspace/TypesAndStructs/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
+}
+
+func main() {
+	user := User {
+		FirstName: "Trevor",
+		LastName: "Sawler",
+	}
+
+	log.Println(user.FirstName, user.LastName, "Birthdate:", user.BirthDate)
+}
+
